eb6c027b0529f981d59f48b5ec49c31ac85abcd9
Adrian Kopciński <kopcinski.a@gmail.com>
2019-04-24 22:22:14 +0200

    added TODO's

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import * as PIXI from 'pixi.js'
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new PIXI.Application();
+
+// TODO Load start page --> (nice animation after loading rectangle) https://www.youtube.com/watch?v=g77WfQ-blxI
+// TODO copy and customize animation animation in pixi.js  -->  https://pixijs.io/examples-v4/#/demos-advanced/slots.js
+// TODO customize start button animation after click in pixi.js
+
+// TODO Gameplay:
+// - load
+// - say hello
+//
+// loop
+// - push button cost c some coins
+// - buton disable and change graphic
+// - animiation start
+// - when 3 wild in row u win <--- TODOS how did u know about the win?
+// - when win scores up
+// repeat
+//
+// TODO how did u know about the win?
+// TODO Animation when win
+// TODO add some coin scores
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
+const app = new PIXI.Application();
+
 document.body.appendChild(app.view);
 
 // load the texture we need
